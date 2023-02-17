b2c582dc84269c0d7556a49777bfb40dfc86f0d3
amorilla42 <andresmm4242@gmail.com>
2023-02-17 14:17:55 +0100

    pruEBA DESDE 42MALAGA

--- src/ft_push_swap.c
+++ src/ft_push_swap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_push_swap.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amorilla <amorilla@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: amorilla <amorilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 20:42:05 by amorilla          #+#    #+#             */
-/*   Updated: 2023/02/12 20:26:11 by amorilla         ###   ########.fr       */
+/*   Updated: 2023/02/17 14:10:45 by amorilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,7 @@ int	main(int argc, char **argv)
 	int		i;
 	t_stack	*a;
 	t_stack	*b;
-
+	
 	i = 0;
 	a = NULL;
 	b = NULL;
